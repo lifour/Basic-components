330fba97cd2fa321202421cc854e682372a7e1b7
ld <1109218203@qq.com>
2024-03-01 23:25:40 +0800

    支持原生按钮的所有属性

--- js/hwButton.jsx
+++ js/hwButton.jsx
@@ -51,7 +51,8 @@ class HwButton extends React.Component {
     }
 
     render() {
-        let { loading, loadingIcon, children } = this.props
+        let props = this.props
+        let { loading, loadingIcon, children } = props
 
         // 设置执行中状态
         let defaultLoadingIcon = <svg viewBox="0 0 1024 1024" className="hw-button-loading-svg" focusable="false" data-icon="loading" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M988 548c-19.9 0-36-16.1-36-36 0-59.4-11.6-117-34.6-171.3a440.45 440.45 0 00-94.3-139.9 437.71 437.71 0 00-139.9-94.3C629 83.6 571.4 72 512 72c-19.9 0-36-16.1-36-36s16.1-36 36-36c69.1 0 136.2 13.5 199.3 40.3C772.3 66 827 103 874 150c47 47 83.9 101.8 109.7 162.7 26.7 63.1 40.2 130.2 40.2 199.3.1 19.9-16 36-35.9 36z"></path></svg>
@@ -59,7 +60,7 @@ class HwButton extends React.Component {
                 { loadingIcon ? loadingIcon : defaultLoadingIcon }
             </span>
 
-        return <button type="button" className={this.className} onClick={this.props?.onClick}>
+        return <button type="button" className={this.className} onClick={props?.onClick} {...props}>
             { loading && loadingSpan }
             <span>{ children || '' }</span>
         </button>
